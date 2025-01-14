0ac8bd6d97c75e79715132f5ac91f0d4a4515dfe
H6f3c <93835294+H6f3c@users.noreply.github.com>
2025-01-14 19:17:38 +0100

    Update yp_scraper.py

--- scrapers/yp_scraper.py
+++ scrapers/yp_scraper.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
-# Configure Chrome options
 chrome_options = Options()
 chrome_options.add_argument('--headless')  # Run in headless mode
-chrome_options.add_argument('--no-sandbox')  # Required in some restricted environments
-chrome_options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
-chrome_options.add_argument('--remote-debugging-port=9222')  # Avoid DevToolsActivePort issue
+chrome_options.add_argument('--no-sandbox')  # Avoid sandboxing issues
+chrome_options.add_argument('--disable-dev-shm-usage')  # Handle limited resources
+chrome_options.binary_location = '/usr/bin/chromium-browser'
 
+driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'), options=chrome_options)
 
 # Automatically download and set up ChromeDriver
 service = Service(ChromeDriverManager().install())
