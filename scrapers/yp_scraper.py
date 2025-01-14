bcc98e0ad9dc0a908d847189dcdda6e25bf7d616
H6f3c <93835294+H6f3c@users.noreply.github.com>
2025-01-14 18:51:43 +0100

    Update yp_scraper.py

--- scrapers/yp_scraper.py
+++ scrapers/yp_scraper.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up headless Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('--headless')  # Run in headless mode
-chrome_options.add_argument('--no-sandbox')  # Required in some restricted environments
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
 chrome_options.add_argument('--remote-debugging-port=9222')  # Avoid DevToolsActivePort issue
+
+# Automatically download and set up ChromeDriver
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Initialize the WebDriver
 driver = webdriver.Chrome(options=chrome_options)
